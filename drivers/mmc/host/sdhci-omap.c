3cacf73c380abc338b5c6090a09d030b9780ee13
Kishon Vijay Abraham I <kishon@ti.com>
2018-02-01 14:24:21 +0530

    mmc: sdhci-omap: Add SPDX identifier
    The SPDX identifier is a legally binding shorthand, which can be used
    instead of the full boiler plate text. Update sdhci-omap driver with the
    correct SPDX license identifier.
    
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>

--- drivers/mmc/host/sdhci-omap.c
+++ drivers/mmc/host/sdhci-omap.c
@@ -1,21 +1,7 @@
-/**
- * SDHCI Controller driver for TI's OMAP SoCs
- *
- * Copyright (C) 2017 Texas Instruments
- * Author: Kishon Vijay Abraham I <kishon@ti.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 of
- * the License as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (c) 2018 Texas Instruments
+// SDHCI Controller driver for TI's OMAP SoCs
+// Author: Kishon Vijay Abraham I <kishon@ti.com>
 
 #include <linux/delay.h>
 #include <linux/mmc/slot-gpio.h>
