b55c6479af402d2240932681215cb936e61ab601
Alan Douglas <adouglas@cadence.com>
2019-12-11 17:06:20 +0530

    PCI: cadence: Add MSI-X support to Endpoint driver
    Implement ->set_msix() and ->get_msix() callback functions in order
    to configure MSIX capability in the PCIe endpoint controller.
    
    Add cdns_pcie_ep_send_msix_irq() to send MSIX interrupts to Host.
    cdns_pcie_ep_send_msix_irq() gets the MSIX table address (virtual
    address) from "struct cdns_pcie_epf" that gets initialized in
    ->set_bar() call back function.
    
    [kishon@ti.com: Re-implement MSIX support in accordance with the
     re-designed core MSI-X interfaces]
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>
    Signed-off-by: Alan Douglas <adouglas@cadence.com>

--- drivers/pci/controller/cadence/pcie-cadence.h
+++ drivers/pci/controller/cadence/pcie-cadence.h
@@ -94,6 +94,7 @@
 #define CDNS_PCIE_EP_FUNC_BASE(fn)	(((fn) << 12) & GENMASK(19, 12))
 
 #define CDNS_PCIE_EP_FUNC_MSI_CAP_OFFSET	0x90
+#define CDNS_PCIE_EP_FUNC_MSIX_CAP_OFFSET	0xb0
 
 /*
  * Root Port Registers (PCI configuration space for the root port function)
@@ -269,6 +270,14 @@ struct cdns_pcie_rc {
 };
 
 /**
+ * struct cdns_pcie_epf - Structure to hold info about endpoint function
+ * @epf_bar: reference to the pci_epf_bar for the six Base Address Registers
+ */
+struct cdns_pcie_epf {
+	struct pci_epf_bar *epf_bar[PCI_STD_NUM_BARS];
+};
+
+/**
  * struct cdns_pcie_ep - private data for this PCIe endpoint controller driver
  * @pcie: Cadence PCIe controller
  * @max_regions: maximum number of regions supported by hardware
@@ -295,6 +304,7 @@ struct cdns_pcie_ep {
 	u64			irq_pci_addr;
 	u8			irq_pci_fn;
 	u8			irq_pending;
+	struct cdns_pcie_epf	*epf;
 };
 
 
